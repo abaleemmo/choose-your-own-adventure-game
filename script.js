40879403662205fabf9b5f28b5a913d8911ece45
Abdul-Aleem Mohammed <abaleemmohammed@gmail.com>
2025-06-11 15:06:58 -0500

    enough

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Declare variables below to save the different divs of your story.
+var playerName;
+var  
 
 
 
@@ -10,6 +12,10 @@
 
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
+
+
+playerName = prompt("What is your name?");
+console.log(playerName);
 
 
 // INSERT_VARIABLE.addEventListener('click', function(){

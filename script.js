e9141d531dda8eaa73005abd5749320fea65cc0e
Abdul-Aleem Mohammed <abaleemmohammed@gmail.com>
2025-06-12 11:24:17 -0500

    company naming added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Declare variables below to save the different divs of your story.
-var companyName;
+var companyNameLoc = document.querySelector(".company-name");
 var startStoryButton = document.querySelector(".start-screen");
 var firstScreen = document.querySelector(".first-screen");
 var namingButton = document.querySelector(".naming-button");
-var secondScreen = document.querySelector(".second-screen");
+var secondScreen = document.getElementsByClassName(".second-screen");
 
 
 
@@ -23,7 +23,10 @@ startStoryButton.addEventListener("click", function(){
 
 namingButton.addEventListener("click", function(){
     companyName = prompt("Enter the name of your company", "Big Startup");
-    secondScreen.style.display = "block";
+    console.log(companyName);
+    // secondScreen.style.display = "block";
+    companyNameLoc.innerHTML = companyName;
+    console.log(companyName);
 });
 
 

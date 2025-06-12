f20943041ff7c3bde8f528ae455c5fe726e5afa3
Abdul-Aleem Mohammed <abaleemmohammed@gmail.com>
2025-06-12 10:33:02 -0500

    added second screen

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Declare variables below to save the different divs of your story.
-var playerName;
-
+var companyName;
+var startStoryButton = document.querySelector(".start-screen");
+var firstScreen = document.querySelector(".first-screen");
+var namingButton = document.querySelector(".naming-button");
+var secondScreen = document.querySelector(".second-screen");
 
 
 
@@ -13,7 +16,15 @@ var playerName;
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
 
+startStoryButton.addEventListener("click", function(){
+    firstScreen.style.display = "block";
+});
 
+
+namingButton.addEventListener("click", function(){
+    companyName = prompt("Enter the name of your company", "Big Startup");
+    secondScreen.style.display = "block";
+});
 
 
 
